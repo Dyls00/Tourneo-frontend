46d1a06ef87e0da30fc38519a0b77faa5e65770a
Dyls <Johane-Arlis.NSANGOUAKANDA@estiam.com>
2025-11-20 15:52:03 +0100

    Merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import "./App.css";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 
+const [view, setView] = useState("tournois");
+
 function App() {
   return (
     <div className="content">
-      <Header />
-      <Main />
+      <Header changeView={setView}/>
+      <Main currentView={view}/>
       <Footer />
     </div>
   );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,29 +1,41 @@
-import type { FC } from "react";
 import { useUser } from "../user";
 import image1 from "../assets/1.png";
+import { TournamentForm } from "./Tournaments/TournamentForm";
+import { TournamentList } from "./Tournaments/tournamentList";
+import { Poules } from "./Poules/Poules";
 
-export const Main: FC = () => {
-  const { user } = useUser();
-  return (
-    <main>
-      <div className="main-content">
-        {!user && (
-          <div className="main-unlogged">
-            <div>
-              <img src={image1} alt="" width={500} />
+type MainProps = {
+    currentView: string;
+};
+
+export const Main = ({ currentView }: MainProps) => {
+    const { user } = useUser();
+
+    return (
+        <main>
+            <div className="main-content">
+
+                {/* SI PAS CONNECTÉ */}
+                {!user && (
+                    <div className="main-unlogged">
+                        <div><img src={image1} alt="" width={500} /></div>
+                        <div className="main-text">
+                            <p>
+                                Tourneo est une application conçue pour faciliter
+                                l’organisation et la gestion de tournois de tennis et de padel amateur.
+                                Elle offre aux organisateurs la possibilité de créer des tournois auto-organisés,
+                                dans lesquels les participants gèrent eux-mêmes leurs matchs.
+                            </p>
+                        </div>
+                    </div>
+                )}
+
+                {/* VUE DYNAMIQUE */}
+                {user && currentView === "tournois" && <TournamentList />}
+                {user && currentView === "tournoiForm" && <TournamentForm />}
+                {user && currentView === "poules" && <Poules />}
+
             </div>
-            <div className="main-text">
-              <p>
-                Tourneo est une application conçue pour faciliter l’organisation
-                et la gestion de tournois de tennis et de padel amateur. Elle
-                offre aux organisateurs la possibilité de créer des tournois
-                auto-organisés, dans lesquels les participants gèrent eux-mêmes
-                leurs matchs.
-              </p>
-            </div>
-          </div>
-        )}
-      </div>
-    </main>
-  );
+        </main>
+    );
 };
